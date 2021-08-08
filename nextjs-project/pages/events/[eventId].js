92251591860639f095da434b04cbb8fc134d2dea
tndms753 <tndms753@naver.com>
2021-08-08 12:27:28 +0900

    add button and event-item

--- nextjs-project/pages/events/[eventId].js
+++ nextjs-project/pages/events/[eventId].js
@@ -1,4 +1,13 @@
+import { useRouter } from "next/router";
+
+import { getEventById } from "../../dummy-data";
+
 function EventDetailPage() {
+  const router = useRouter();
+
+  const eventId = router.query.eventId;
+  getEventById();
+
   return (
     <div>
       <h1>Event Detail</h1>
